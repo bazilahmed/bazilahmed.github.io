cc2a42dd893097a5397e706c231bcc8d165d45a9
Bazil Ahmed <bazilahmed.usa@gmail.com>
2019-03-14 00:23:23 -0400

    moved the bio files to bio-website folder and added the pages for the yelp project website

---
+++ app.js
@@ -0,0 +1,58 @@
+
+    const width = 960;
+    const height = 500;
+      const config = {
+      speed: 0.005,
+      verticalTilt: -30,
+      horizontalTilt: 0
+    }
+    let locations = [];
+    const svg = d3.select('svg')
+        .attr('width', width).attr('height', height);
+    const markerGroup = svg.append('g');
+    const projection = d3.geoOrthographic();
+    const initialScale = projection.scale();
+    const path = d3.geoPath().projection(projection);
+    const center = [width/2, height/2];
+
+    drawGlobe();    
+    drawGraticule();
+    enableRotation();    
+
+    function drawGlobe() {  
+        d3.queue()
+            .defer(d3.json, 'https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/world-110m.json')          
+            .defer(d3.json, 'locations.json')
+            .await((error, worldData, locationData) => {
+                svg.selectAll(".segment")
+                    .data(topojson.feature(worldData, worldData.objects.countries).features)
+                    .enter().append("path")
+                    .attr("class", "segment")
+                    .attr("d", path)
+                    .style("stroke", "#888")
+                    .style("stroke-width", "1px")
+                    .style("fill", (d, i) => '#e5e5e5')
+                    .style("opacity", ".6");                 
+            });
+    }
+
+    function drawGraticule() {
+        const graticule = d3.geoGraticule()
+            .step([10, 10]);
+
+        svg.append("path")
+            .datum(graticule)
+            .attr("class", "graticule")
+            .attr("d", path)
+            .style("fill", "#fff")
+            .style("stroke", "#ccc");
+    };
+
+    function enableRotation() {
+        d3.timer(function (elapsed) {
+            projection.rotate([config.speed * elapsed - 120, config.verticalTilt, config.horizontalTilt]);
+            svg.selectAll("path").attr("d", path);
+            // drawMarkers();
+        });
+    
+    };
